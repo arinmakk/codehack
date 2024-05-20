69952306b32c53a67f051227f402ee2812430d18
arinmakk <arinmakk@gmail.com>
2024-05-20 16:19:39 +0800

    added giant red text

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+
+      <Text style={{ fontSize: 50, color: "red", textAlign:'center'}}>
+        Hellooooo World
+      </Text>
+      
       <StatusBar style="auto" />
     </View>
   );
